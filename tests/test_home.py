adad17810072fe859dffaa9fb50a647abbf1afb6
b63 <avink5760@gmail.com>
2019-10-29 10:35:14 -0500

    reorganized directory

---
+++ tests/test_home.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import tempfile
+import pytest
+
+from wordstree import app, init_db, get_db
+
+
+@pytest.fixture
+def client():
+    db, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        with app.app_context():
+            init_db()
+        yield client
+
+    os.close(db)
+    os.unlink(app.config['DATABASE'])
+
+
+def test_homepage(client):
+    """Test request to root path returns something resembling what it should. """
+    data = client.get('/').data
+    import sys
+    print(sys.path)
+
+    assert b'canvas' in data
--- wordstree/schema.sql
+++ wordstree/schema.sql
@@ -1,8 +1,8 @@
 create table branches (
   id integer primary key autoincrement,
-  'text' text not null,
-  depth integer not null,
-  index integer not null,
+  "text" text not null,
+  "depth" not null,
+  "index" integer not null,
   owner_id integer not null,
   price integer not null,
   color text not null,
