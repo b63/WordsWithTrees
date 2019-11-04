666ef9d65e6556584a68c6f2fd6fb95692b2e392
Ted Yap <tyap@iwu.edu>
2019-11-03 21:38:22 -0600

    set home page as login required

--- wordstree/home.py
+++ wordstree/home.py
@@ -1,6 +1,6 @@
 from wordstree.db import get_db
 from sqlite3 import dbapi2 as sqlite3
-from flask import Blueprint, Flask, request, g, redirect, url_for, render_template, flash, current_app
+from flask import Blueprint, Flask, request, g, redirect, url_for, render_template, flash, current_app, session
 from werkzeug.security import generate_password_hash
 
 bp = Blueprint('root', __name__)
@@ -10,6 +10,8 @@ bp = Blueprint('root', __name__)
 @bp.route('/')
 def home():
     """ Handles requests to the root page """
+    if session.get("user_id") is None:
+        return redirect(url_for("login.login_page"))
     return render_template('home.html')
 
 
--- wordstree/login.py
+++ wordstree/login.py
@@ -10,6 +10,7 @@ bp = Blueprint('login', __name__)
 def login_form():
     """ Login user with a form. """
 
+    session.clear()
     # ensure username was submitted
     if not request.form.get("username"):
         flash("You must provide a username")
@@ -22,16 +23,16 @@ def login_form():
 
     # query database for username
     db = get_db()
-    cur = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+    cur = db.execute("SELECT * FROM users WHERE username = (?)", [request.form.get("username")])
     user_data = cur.fetchone()
 
     # ensure username exists and password is correct
-    if len(user_data) != 1 or not check_password_hash(user_data[0]["hash_password"], request.form.get("password")):
+    if user_data is None or not check_password_hash(user_data["hash_password"], request.form.get("password")):
         flash("Invalid username and/or password")
         return redirect(url_for('login.login_page'))
 
     # remember which user has logged in
-    session["user_id"] = user_data[0]["id"]
+    session["user_id"] = user_data["id"]
 
     # redirect user to home page
     return redirect(url_for("root.home"))
@@ -51,4 +52,4 @@ def logout():
     session.clear()
 
     # redirect user to login form
-    return redirect(url_for("login_page"))
+    return redirect(url_for("login.login_page"))
