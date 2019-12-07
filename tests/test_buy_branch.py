42d2525dac02e0129d64058e2175c34d83dc3f7f
Nico Lopez <dlopez1@iwu.edu>
2019-12-07 14:15:19 -0600

    buy branch updates

--- tests/test_buy_branch.py
+++ tests/test_buy_branch.py
@@ -1,6 +1,5 @@
 from wordstree.db import init_db, get_db
 
-
 def signup_login(client):
     return client.post('/signup', data={
         'name': 'Person',
@@ -13,6 +12,7 @@ def insert_branch(app, text, depth, ind, owner_id, sell):
     with app.app_context():
         db = get_db()
         cur = db.cursor()
+
         db.execute(
             'INSERT INTO branches (ind, depth, length, width, angle, pos_x, pos_y, tree_id) VALUES'
             '(?, ?, ?, ?, ?, ?, ?, 1)',
@@ -27,11 +27,17 @@ def insert_branch(app, text, depth, ind, owner_id, sell):
         db.commit()
 
 
-def test_view_inventory(client, app):
-    signup_login(client)
-    insert_branch(app, "Branch 1", 1, 1, 1, 0)
-    insert_branch(app, "Branch 2", 1, 2, 1, 0)
-    rv = client.get('/inventory', follow_redirects=True)
-    point = rv.data
-    assert b'Branch 1' in point
-    assert b'Branch 2' in point
+def test_buy_branches(client, app):
+    with app.app_context():
+        print("jg")
+        signup_login(client)
+        insert_branch(app, "Branch 1", 1, 1, 1, 0)
+        insert_branch(app, "Branch 2", 1, 2, 1, 1)
+        rv = client.post('/buy ', data={
+            'new-db': 'new Branch',
+            'branch_id': 2
+        }, follow_redirects=True)
+        db = get_db()
+        # print("hello there")
+        # cur = db.execute('SELECT text FROM branches_ownership WHERE id=1')
+        #
