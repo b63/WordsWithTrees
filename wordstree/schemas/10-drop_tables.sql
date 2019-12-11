013b722f9e2a5abac2aab1f101c63577bead3767
b63 <avink5760@gmail.com>
2019-12-10 21:04:22 -0600

    merged with tree branch

--- wordstree/schemas/10-drop_tables.sql
+++ wordstree/schemas/10-drop_tables.sql
@@ -1,6 +1,12 @@
 drop table if exists branches_ownership;
 drop table if exists branches;
+
 drop table if exists tiles;
+
 drop table if exists zoom_info;
 drop table if exists tree;
+
 drop table if exists users;
+
+drop table if exists notification_objects;
+drop table if exists notifications;
