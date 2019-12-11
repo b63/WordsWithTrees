e327b635054f50ba7cb91767489d84f3b465247c
b63 <avink5760@gmail.com>
2019-12-10 21:22:32 -0600

    updated drop commands

--- wordstree/schemas/10-drop_tables.sql
+++ wordstree/schemas/10-drop_tables.sql
@@ -6,7 +6,8 @@ drop table if exists tiles;
 drop table if exists zoom_info;
 drop table if exists tree;
 
-drop table if exists users;
-
 drop table if exists notification_objects;
 drop table if exists notifications;
+
+drop table if exists users;
+
